d0a5d36b761e4d64f0a89e65b9dc362d2a7234bc
arraytools <arraytools@gmail.com>
2015-09-08 09:00:38 -0400

    change dos format on Rcommand.bat

--- windows/3.2.2/Rcommand.bat
+++ windows/3.2.2/Rcommand.bat
@@ -1,8 +1,5 @@
 set PATH=c:\Rtools\bin;c:\Rtools\gcc-4.6.3\bin
-
 set path=%PATH%;c:\users\brb\downloads\r-3.2.2\bin\x64
-
 set PATH=%PATH%;C:\users\brb\downloads\miktex\miktex\bin
-
 set PATH=%PATH%;C:\windows;c:\windows\system32
 
